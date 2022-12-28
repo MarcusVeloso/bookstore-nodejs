3b0915eb79230d1c19be1950ec60c15c7a28b454
Marcus Veloso <desenvolvedoroficial@gmail.com>
2022-12-28 16:50:21 -0300

    feat: add get book by id

--- src/app.js
+++ src/app.js
@@ -18,6 +18,11 @@ app.get("/books", (req, res) => {
   res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+  let index = searchBook(req.params.id);
+  res.json(books[index]);
+});
+
 app.post("/books", (req, res) => {
   books.push(req.body);
   res.status(201).send("Success adding new book");
