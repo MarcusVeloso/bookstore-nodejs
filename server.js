4ed3319760d4270f36e3d61b62bdf57f12832e9a
Marcus Veloso <desenvolvedoroficial@gmail.com>
2022-12-28 14:26:48 -0300

    feat: add routes

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const http = require("http")
 
 const port = 3000;
 
+const routes = {
+  '/': 'Node Course',
+  '/books': 'Books list',
+  '/authors': 'Authors list',
+  '/publishers': 'Publishers list',
+  '/about': 'About page',
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Bookstore Node')
+  res.end(routes[req.url])
 })
 
 server.listen(port, () => {
